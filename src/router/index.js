e5e9a9bfaa60b5705a5527ced59cb96fbe75c5fe
Bill2926 <nguyenducmanh2926@gmail.com>
2026-05-04 23:42:50 +0700

    Done contact page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AboutMeView from '@/views/AboutMeView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
 import ProjectsView from '@/views/ProjectsView.vue'
+import ContactView from '@/views/ContactView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: "/projects",
       name: 'Projects',
       component: ProjectsView
+    },
+    {
+      path: "/contact",
+      name: 'Contact',
+      component: ContactView
     },
     {
       path: '/:pathMatch(.*)*',
---
+++ src/views/ContactView.vue
@@ -0,0 +1,31 @@
+<script setup></script>
+
+<template>
+  <section class="d-flex align-items-center justify-content-center py-5">
+    <div class="row justify-content-center align-items-center g-2">
+      <div class="col-12 col-lg-4 text-center">
+        <p class="display-4">Contact</p>
+        <p class="lead">
+          I'd love to broaden my connection so feel free to contact me at anytime:)
+        </p>
+      </div>
+      <div class="col-12 col-lg-4 d-flex flex-row flex-md-column justify-content-center align-items-center gap-2">
+        <a
+          href="https://www.instagram.com/_simply.duck/"
+          class="text-reset"
+          target="_blank"
+          ><i class="bi bi-instagram fs-1"></i
+        ></a>
+        <a
+          href="https://www.linkedin.com/in/%C4%91%E1%BB%A9c-m%E1%BA%A1nh-nguy%E1%BB%85n-811613328/"
+          class="text-reset"
+          target="_blank"
+          ><i class="bi bi-linkedin fs-1"></i
+        ></a>
+        <a href="https://github.com/Bill2926" class="text-reset" target="_blank"
+          ><i class="bi bi-github fs-1"></i
+        ></a>
+      </div>
+    </div>
+  </section>
+</template>
