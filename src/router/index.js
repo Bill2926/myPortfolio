51915e4b26bf3cf62cdb3fa86c1179e1f6ae2f01
Bill2926 <nguyenducmanh2926@gmail.com>
2026-05-13 23:16:59 +0700

    Added HashHistory for deployment

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import AboutMeView from '@/views/AboutMeView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
 import ProjectsView from '@/views/ProjectsView.vue'
@@ -6,7 +6,7 @@ import ContactView from '@/views/ContactView.vue'
 import PlansView from '@/views/PlansView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
